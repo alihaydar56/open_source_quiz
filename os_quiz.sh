93708f2313bd7c7bc96335929211367841a2e606
Ali Haydar Sevgili <haydar.sevgili@ideabilisim.com>
2021-12-28 11:25:23 +0300

    open source first question

---
+++ os_quiz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+               
+read -p "Enter a number: " num
+
+if [ $((num%2)) -ne 0 ]
+then
+ for((i=1; i<=num; i++))
+ do
+    for((j=1; j<=num - i; j++))
+    do
+        echo -n "  "
+    done
+    for((j=1; j<=2*i - 1; j++))
+    do
+        echo -n "* "
+    done
+    echo
+done
+ else
+   echo "value not perfect"
+fi
